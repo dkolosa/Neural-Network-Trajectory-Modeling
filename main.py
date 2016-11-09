2b0458a3be0d29a1cfaaf19684d46173d0afecc5
Daniel Kolosa <contact@dkolosa.com>
2016-11-09 18:36:58 -0500

    Fixed final delta V

--- main.py
+++ main.py
@@ -37,14 +37,18 @@ def main():
 
     #Assume a known state vector (from curtis)
 
+    # Delete file contents
+    # training_File_Input = open("trainingData.csv", 'w')
+    # training_File_Input.close()
+
     # generate a random set of chaser and target values
-    for x in range(1, 11):
+    for x in range(100):
         # Space Station
-        r0random = np.random.uniform(low=100, high=10000.0, size=3)
+        r0random = np.random.uniform(low=100, high=1000.0, size=3)
         v0random = np.random.uniform(low=-3.0, high=3.0, size=3)
 
         # range for random value generation
-        rdiff = 20
+        rdiff = 30
         vdiff = 0.1
 
         # Spacecraft
@@ -56,14 +60,14 @@ def main():
                                 np.random.uniform(low=v0random[1] - vdiff, high=v0random[1] + vdiff),
                                 np.random.uniform(low=v0random[2] - vdiff, high=v0random[2] + vdiff)])
 
-        """
-        Space station (target vechicle)
-        r0 = np.array([1622.39, 5305.10, 3717.44])
-        v0 = np.array([-7.29977, 0.492357, 2.48318])
-        # Chaser
-        rtarg = np.array([1612.5, 5310.19, 3750.33])
-        vtarg = np.array([-7.35321, 0.463856, 2.46920])
-        """
+
+        # # Space station (target vechicle)
+        # r0 = np.array([1622.39, 5305.10, 3717.44])
+        # v0 = np.array([-7.29977, 0.492357, 2.48318])
+        # # Chaser
+        # rtarg = np.array([1612.75, 5310.19, 3750.33])
+        # vtarg = np.array([-7.35321, 0.463856, 2.46920])
+
 
         # space Station
         r0, v0 = r0random, v0random
@@ -85,8 +89,7 @@ def main():
         deltar0 = Q.dot(deltar)
         deltav0 = Q.dot(deltav)
 
-
-        #time of the 2-impulse maneuver
+        # time of the 2-impulse maneuver
         t = 8 * 60**2  # hours to seconds
 
         # matrix form CW
@@ -95,7 +98,7 @@ def main():
                       0, 0, np.cos(n*t)]).reshape((3, 3))
         rv = np.array([(1/n*np.sin(n*t)), 2/n*(1-np.cos(n*t)), 0,
                        2/n*(np.cos(n*t)-1), (1/n)*(4*np.sin(n*t)-3*n*t), 0,
-                       0, 0, 1/n*np.sign(n*t)]).reshape((3, 3))
+                       0, 0, 1/n*np.sin(n*t)]).reshape((3, 3))
         vr = np.array([3*n*np.sin(n*t), 0, 0,
                       6*n*(np.cos(n*t)-1), 0, 0,
                       0, 0, -n*np.sin(n*t)]).reshape((3, 3))
@@ -103,34 +106,30 @@ def main():
                        -2*np.sin(n*t), 4*np.cos(n*t)-3, 0,
                        0, 0, np.cos(n*t)]).reshape((3, 3))
 
-        # First CW equation deltarf
-        deltarf = rr.dot(deltar0) + rv.dot(deltav0)
+        deltav0p = -np.linalg.inv(rv).dot(np.dot(rr, deltar0))
+
+        # First CW equation deltarf is assumed zero
+        # deltarf = rr.dot(deltar0) + rv.dot(deltav0)
         # Second CW equation deltavf
-        deltavf = vr.dot(deltar0) + vv.dot(deltav0)
+        deltavf = vr.dot(deltar0) + vv.dot(deltav0p)
 
-        deltarv0 = np.concatenate((deltar0, deltav0)).reshape((1, 6))
-        deltarvf = np.concatenate((deltarf, deltavf)).reshape((1, 6))
+        deltarv0 = np.concatenate((deltar0, deltavf)).reshape((2, 3))
+        # deltarvf = np.concatenate((deltarf, deltavf)).reshape((1, 6))
 
-        delta = np.concatenate(([deltarv0], [deltarvf]))
-        print(delta, "\n", "iter: ", x)
         # Save the initial and final states in the csv file
 
-        # trainingFileInput = open("trainingData.csv", 'ab')
-        # np.savetxt(trainingFileInput, delta, delimiter=',')
-        # trainingFileInput.close()
+        # training_File_Input = open("trainingData.csv", 'ab')
+        # np.savetxt(training_File_Input, deltarv0, delimiter=',')
+        # training_File_Input.close()
 
-        # trainingFileOutput = open("trainingDataOutput.csv", 'ab')
-        # np.savetxt(trainingFileOutput, deltarvf, delimiter=',')
-        # trainingFileOutput.close()
+        # This is the test data, comment out if generating data for training
+        testFileInput = open("testData.csv", 'ab')
+        np.savetxt(testFileInput, np.reshape(deltar0, (1, 3)), delimiter=',')
+        testFileInput.close()
 
-        # # This is the test data, comment out if generating data for training
-        # testFileInput = open("testData.csv", 'ab')
-        # np.savetxt(testFileInput, deltarv0, delimiter=',')
-        # testFileInput.close()
-        #
-        # testFileOutput = open("outputTestData.csv", 'ab')
-        # np.savetxt(testFileOutput, deltarvf, delimiter=',')
-        # testFileOutput.close()
+        testFileOutput = open("outputTestData.csv", 'ab')
+        np.savetxt(testFileOutput, np.reshape(deltavf, (1, 3)), delimiter=',')
+        testFileOutput.close()
 
         del r0random, v0random, rtargrandom, vtargrandom
     pass
